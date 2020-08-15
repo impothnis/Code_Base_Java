7a5c11ea5641b34510ce29989f329c8cf7384388
impothnis <pavanpothnis@outlook.com>
2020-08-15 23:02:05 +0530

    First Commit

---
+++ Coding2020/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		if(Boolean.FALSE.equals(true) && (false || true) )
+		{
+				System.out.println("Pavan");
+		}
+
+
+	}
+
+}
